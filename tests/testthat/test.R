af78d40b4090d76473c3c9bd3725efc0383af779
beanumber <ben.baumer@gmail.com>
2016-01-05 21:17:24 -0500

    moved crosscheck to openWAR

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,10 +1,6 @@
 library(openWARData)
 
 
-test_that("crosscheck works", {
-  expect_is(crosscheck(MLBAM2012), "numeric")
-})
-
 test_that("data are tbls", {
   expect_is(MLBAM2012, "GameDayPlays")
   expect_is(idTT, "tbl_df")
